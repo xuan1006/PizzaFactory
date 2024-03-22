ba74b90979bc077c36826acb4fd8037064039a90
xuan1006 <bob921006@gmail.com>
2024-03-23 03:08:25 +0800

    two commit

---
+++ src/main/java/org/example/ChicagoPizzaStore.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import Chicago.ChicagoStyleCheesePizza;
+import Chicago.ChicagoStyleClamPizza;
+import Chicago.ChicagoStylePepperoniPizza;
+import Chicago.ChicagoStyleVeggiePizza;
+
+public class ChicagoStylePizzaStore extends PizzaStore{
+    @Override
+    protected Pizza createPizza(String item) {
+       if (item.equals("cheese")){
+           return new ChicagoStyleCheesePizza();
+       }else if (item.equals("veggie")){
+           return new ChicagoStyleVeggiePizza();
+       }else if (item.equals("clam")){
+           return new ChicagoStyleClamPizza();
+       }else if (item.equals("pepperoni")){
+           return new ChicagoStylePepperoniPizza();
+       }else return null;
+    }
+}
